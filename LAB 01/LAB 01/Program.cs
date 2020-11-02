224362d93aee0183718d0cdfdad3819cb1f3f4ca
Shala <s00043039@acad.tri-c.edu>
2020-11-01 20:48:35 -0500

    Completed Lab 2

--- LAB 01/LAB 01/Program.cs
+++ LAB 01/LAB 01/Program.cs
@@ -2,11 +2,21 @@
 
 namespace HelloWorld
 {
-    class Program
+    class Multiplication    
     {
+        public static int Brown { get; private set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        }
-    }
-}
+        int number 1; //declare first number to add 
+            int number 2; //declare second number to add
+            int sum; //declare sum of number 1 and number 2
+            object p = Console.Writeline("Hello World", from[Shalanda Brown]);
+            Console.Write(HelloWorld!           From[Shalanda Brown]);
+// the main method is the entry point
+// integer is a number without a decimal and float is a number with decimal
+// account class with an Instance variable and A property
+// get and set accessors
+// class is keyword declaration and object is the name in the clas
+
